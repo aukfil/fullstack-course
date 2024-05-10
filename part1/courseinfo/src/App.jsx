200506a9efcad374459d42fa33b1e40f9d38b3cb
auksztol <bigiottini@gmail.com>
2024-05-10 21:03:50 +0200

    Add solution for exercise 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -34,9 +34,10 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
+  const course = { 
+    name: 'Half Stack application development',
+    parts: [
+  {
     name: 'Fundamentals of React',
     exercises: 10
   },
@@ -49,14 +50,17 @@ const App = () => {
     exercises: 14
   }
 ]
+}
+
 
   return (
     <div>
-      <Header course = {course} />
-      <Content parts = {parts} />
-      <Total parts = {parts} />
+      <Header course = {course.name} />
+      <Content parts = {course.parts} />
+      <Total parts = {course.parts} />
     </div>
   )
 }
+
 
 export default App
