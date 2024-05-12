82fc2ba17d410efeb7d02504db498762d4d8cc07
auksztol <bigiottini@gmail.com>
2024-05-12 13:21:11 +0200

    Add header component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from 'react'
 
+const Header = (props) => {
+  return (
+    <div>
+    <h1>{props.header}</h1>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const header = 'give feedback'
+
   return (
     <div>
-      code here
+      <Header header = {header} />
     </div>
   )
 }
