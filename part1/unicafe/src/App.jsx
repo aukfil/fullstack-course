a012fcf00b565b980a9d4f3159de15100f4945f7
auksztol <bigiottini@gmail.com>
2024-05-14 16:15:04 +0200

    Add solution to exercise 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -16,9 +16,22 @@ const App = () => {
 
   const header = 'give feedback'
 
+  const handleClick = () => {
+    console.log('clicked')
+  }
+
   return (
     <div>
       <Header header = {header} />
+      <div>
+        <button> good {good}</button>
+        <button> neutral {neutral}</button>
+        <button> bad {bad}</button>
+      </div>
+      <h1>statistics</h1>
+      <div>good</div>
+      <div>neutral</div>
+      <div>bad</div>
     </div>
   )
 }
