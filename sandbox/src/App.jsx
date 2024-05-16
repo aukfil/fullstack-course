83a69bac966d13c6f7dadc076dfc7add2674c95b
auksztol <bigiottini@gmail.com>
2024-05-16 22:44:58 +0200

    Add sandbox

---
+++ sandbox/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  const handleClick = () => {
+    console.log('clicked')
+  }
+
+  return (
+    <div>
+    <div>{counter}</div>
+    <button onClick = {handleClick}>
+      plus
+    </button>
+    </div>
+  )
+}
+
+export default App
