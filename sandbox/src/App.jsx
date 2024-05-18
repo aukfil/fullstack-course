95e964f50aee653431d746451ea4adb588b55625
auksztol <bigiottini@gmail.com>
2024-05-18 17:37:05 +0200

    Update sandbox

--- sandbox/src/App.jsx
+++ sandbox/src/App.jsx
@@ -1,15 +1,36 @@
 import { useState } from 'react'
 
+const Display = (props) => {
+  return(
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return(
+    <div>
+      <button onClick = {props.onClick}>
+        {props.text}
+      </button>
+    </div>
+  )
+}
+
 const App = () => {
   const [counter, setCounter] = useState(0)
 
+  const increaseByOne = () => setCounter(counter +1)
+
+  const setToZero = () => setCounter(0)
+
   return (
     <div>
-    <div>{counter}</div>
-    <button onClick = {() => setCounter(counter + 1)}>
+    <Display counter={counter} />
+    <Button text = {'plus'} onClick = {increaseByOne} />
+    <button onClick = {increaseByOne}>
       plus
     </button>
-    <button onClick = {() => setCounter(0)}>
+    <button onClick = {setToZero}>
       zero
     </button>
     </div>
