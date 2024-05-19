44c662b78b358ed2aea82a57349a140c6c1c0d91
auksztol <bigiottini@gmail.com>
2024-05-19 23:04:36 +0200

    Update sandbox

--- sandbox/src/App.jsx
+++ sandbox/src/App.jsx
@@ -1,24 +1,31 @@
 import { useState } from 'react'
 
-const Display = ({counter}) => <div> {counter} </div>
-
-const Button = ({onClick, text}) => <button onClick = {onClick}> {text} </button>
-
 const App = () => {
-  const [counter, setCounter] = useState(0)
+  const [clicks, setClicks] = useState({left: 0, right: 0})
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
+  const handleLeftClick = () => {
+    const newClicks = { 
+      left: clicks.left + 1, 
+      right: clicks.right 
+    }
+    setClicks(newClicks)
+  }
 
-  const setToZero = () => setCounter(0)
+  const handleRightClick = () => {
+    const newClicks = { 
+      left: clicks.left, 
+      right: clicks.right + 1 
+    }
+    setClicks(newClicks)
+  }
 
   return (
     <div>
-    <Display counter={counter} />
-    <Button text = {'plus'} onClick = {increaseByOne} />
-    <Button text = {'zero'} onClick = {setToZero} />
-    <Button text = {'minus'} onClick = {decreaseByOne} />
-    </div>
+    {clicks.left}
+    <button onClick={handleLeftClick}>left</button>
+    <button onClick={handleRightClick}>right</button>
+    {clicks.right}
+  </div>
   )
 }
 
