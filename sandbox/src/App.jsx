6afa37140928cede064f3b5f6472cb25a207499d
auksztol <bigiottini@gmail.com>
2024-05-20 14:00:24 +0200

    Update sandbox

--- sandbox/src/App.jsx
+++ sandbox/src/App.jsx
@@ -3,21 +3,11 @@ import { useState } from 'react'
 const App = () => {
   const [clicks, setClicks] = useState({left: 0, right: 0})
 
-  const handleLeftClick = () => {
-    const newClicks = { 
-      left: clicks.left + 1, 
-      right: clicks.right 
-    }
-    setClicks(newClicks)
-  }
+  const handleLeftClick = () =>
+    setClicks({...clicks, left: clicks.left + 1})
 
-  const handleRightClick = () => {
-    const newClicks = { 
-      left: clicks.left, 
-      right: clicks.right + 1 
-    }
-    setClicks(newClicks)
-  }
+  const handleRightClick = () =>
+    setClicks({...clicks, right: clicks.right + 1})
 
   return (
     <div>
