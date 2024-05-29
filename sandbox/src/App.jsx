1266779621451c95789cdc5e04ac0c4148b787bf
auksztol <bigiottini@gmail.com>
2024-05-29 16:53:35 +0200

    Update sandbox

--- sandbox/src/App.jsx
+++ sandbox/src/App.jsx
@@ -1,50 +1,17 @@
-import { useState } from 'react'
+const App = (props) => {
+  const { notes } = props
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
   return (
     <div>
-      button press history: {props.allClicks.join(' ')}
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <li key={note.id}>
+            {note.content}
+          </li>
+        )}
+      </ul>
     </div>
-  )
-}
-
-const Button = ({handleClick, text}) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
-
-const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-  }
-
-  return (
-    <div>
-    {left}
-    <Button handleClick={handleLeftClick} text='left' />
-    <Button handleClick={handleRightClick} text='right' />
-    {right}
-    <History allClicks = {allClicks} />
-  </div>
   )
 }
 
