faeff7ff51a312c29ef68de243838f44bc680ab0
auksztol <bigiottini@gmail.com>
2024-06-04 17:24:17 +0200

    Update 2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,26 @@
 import { useState } from 'react'
 
 const App = () => {
+  
+  // Array to store people's names, initializen with a first name
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
+
   const [newName, setNewName] = useState('')
+
+  // Event handler for the form submission event (onSubmit)
+  const addName = (event) => {
+    event.preventDefault()
+    console.log('Button clicked', event.target)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName}/>
         </div>
         <div>
           <button type="submit">add</button>
