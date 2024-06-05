1bb2079d4893b551102f0c8152f26ae8751ddb1b
auksztol <bigiottini@gmail.com>
2024-06-05 11:07:48 +0200

    Add solution to 2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,36 @@
 import { useState } from 'react'
 
+// Initialize the ID counter
+let nextId = 2
+
 const App = () => {
   
-  // Array to store people's names, initializen with a first name
+  // Array to store people's names, initialized with a first name
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { id: 1,
+      name: 'Arto Hellas'
+     }
   ]) 
 
+  // State for storing input submitted by the user - controlling input with state variable
   const [newName, setNewName] = useState('')
 
   // Event handler for the form submission event (onSubmit)
   const addName = (event) => {
     event.preventDefault()
-    console.log('Button clicked', event.target)
+    const nameObject = {
+      id: nextId++,
+      name: newName
+    }
+
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  // Event handler for synchronising every change to the input with the newName state
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
   }
 
   return (
@@ -20,14 +38,17 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
         <div>
-          name: <input value={newName}/>
+          name: <input 
+          value={newName}
+          onChange={handleNameChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>{persons.map(person => <div key={person.name}>{person.name}</div>)}</div>
     </div>
   )
 }
