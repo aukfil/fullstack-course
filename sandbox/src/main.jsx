4214123c41b9a385979a7963022e7af39db2a5da
auksztol <bigiottini@gmail.com>
2024-06-09 20:10:07 +0200

    Update sandbox

--- sandbox/src/main.jsx
+++ sandbox/src/main.jsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client'
 import axios from 'axios'
 
 const promise = axios.get('http://localhost:3001/notes')
-console.log(promise)
+
+promise.then(response => {
+  console.log(response)
+})
 
 const promise2 = axios.get('http://localhost:3001/foobar')
 console.log(promise2)
