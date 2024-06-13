5f060abe373833a7dc97e44e10e2ef16a1d7bdf9
auksztol <bigiottini@gmail.com>
2024-06-13 18:32:36 +0200

    Update sandbox

--- sandbox/src/App.jsx
+++ sandbox/src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   const [newNote, setNewNote] = useState()
   const [showAll, setShowAll] = useState(true)
 
-  const hook = () => {
+  useEffect(() => {
     console.log('effect')
     axios
       .get('http://localhost:3001/notes')
@@ -15,9 +15,7 @@ const App = () => {
         console.log('promise fulfilled')
         setNotes(response.data)
       })
-  }
-  
-  useEffect(hook, [])
+  }, [])
 
   console.log('render', notes.length, 'notes')
 
