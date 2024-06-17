48368e08f8ef263b8b1f1b690927ec56617d1437
auksztol <bigiottini@gmail.com>
2024-06-17 18:30:11 +0200

    Update sandbox

--- sandbox/src/App.jsx
+++ sandbox/src/App.jsx
@@ -29,7 +29,8 @@ const App = () => {
     axios
     .post('http://localhost:3001/notes', noteObject)
     .then(response => {
-      console.log(response)
+      setNotes(notes.concat(response.data))
+      setNewNote('')
     })
   }
 
@@ -42,6 +43,17 @@ const App = () => {
     ? notes
     : notes.filter(note => note.important)
 
+
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(n => n.id !== id ? n : response.data))
+    })
+  }
+
   return (
     <div>
       <h1>Notes</h1>
@@ -51,7 +63,13 @@ const App = () => {
         </button>
       </div>
       <ul>
-        {notesToShow.map(note => <Note key={note.id} note={note} />)}
+        {notesToShow.map(note => 
+          <Note 
+            key={note.id} 
+            note={note} 
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        )}
       </ul>
       <form onSubmit={addNote}>
         <input 
