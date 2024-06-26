08b351a21d6babfd0fb056efbe195eca4006184d
auksztol <bigiottini@gmail.com>
2024-06-26 14:05:04 +0200

    Update sandbox

--- sandbox/src/App.jsx
+++ sandbox/src/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState()
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened')
 
   useEffect(() => {
     noteService
@@ -54,16 +55,32 @@ const App = () => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch(error => {
-        alert(
-          `the note ${note.content} was already deleted from server`
+        setErrorMessage(
+          `Note '${note.content}' was already removed from the server`
         )
+        setTimeout( () => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
+  }
+
+  const Notification = ({ message }) => {
+    if (message === null) {
+      return null
+    }
+
+    return (
+      <div className = 'error'>
+        {message}
+      </div>
+    )
   }
 
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message = {errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
