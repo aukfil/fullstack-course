35ddf7281220a90620ef16af4733cbbcb0b6c42f
auksztol <bigiottini@gmail.com>
2024-07-08 22:16:02 +0200

    Add solution to 2.18

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -8,15 +8,36 @@ const Filter = ({ filter, handleFilterChange }) => (
 )
 
 // Countries rendering component
-const Countries = ({ countriesToShow }) => (
-  <div>
-    {countriesToShow.map(country =>
-      <div key={country.cca3}>
-        {country.name.common}
+const Countries = ({ countriesToShow }) => {
+  if (countriesToShow.length > 10) {
+    return(<div>Too many matches, specify another filter </div>)
+  } else if (countriesToShow.length === 1) {
+    const country = countriesToShow[0]
+    return(
+      <div>
+        <h2>{country.name.common}</h2>
+        <p>Capital: {country.capital}</p>
+        <p>Area: {country.area}</p>
+        <h3>Languages:</h3>
+        <ul>
+          {Object.values(country.languages).map(language =>
+            <li key={language}>{language}</li>
+          )}
+        </ul>
+        <img src={country.flags.png} alt={`Flag of ${country.name.common}`} />
       </div>
-    )}
-  </div>
-)
+    )
+  } else { 
+    return(
+      <div>
+        {countriesToShow.map(country =>
+          <div key={country.cca3}>
+            {country.name.common}
+          </div>
+        )}
+      </div>
+  )}
+}
 
 const App = () => {
 
