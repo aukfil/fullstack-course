a3ff6f0835732c54bfc58736485180ce3f50241e
auksztol <bigiottini@gmail.com>
2024-07-14 13:22:39 +0200

    Update 2.20

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -24,7 +24,11 @@ const Weather = ({ country, lat, lon }) =>{
       {weather ? (
         <div>
         <p>Temperature: {weather.main.temp} C</p>
-        <p>Wind: {weather.wind.speed}</p>
+        <img
+          src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+          alt={weather.weather[0].description}
+        />
+        <p>Wind: {weather.wind.speed} m/s</p>
         </div>
       ) : (
         <p>Loading weather data...</p>
