c19ff3fb793bc4e0f89eb940f2617ea027056ae6
auksztol <bigiottini@gmail.com>
2024-09-25 13:52:42 +0200

    Add error message for name validation

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -125,6 +125,10 @@ const App = () => {
         setNewName('')
         setNewNumber('')
       }) 
+      .catch(error => {
+        console.log(error.response.data.error)
+        setErrorMessage(error.response.data.error)
+      })
     }
   }
 
