023f410fcbe99fecd5ea497c61af0c3a7f9f1f71
auksztol <bigiottini@gmail.com>
2024-11-04 13:59:03 +0100

    Add note api test

--- notes-backend/tests/note_api.test.js
+++ notes-backend/tests/note_api.test.js
@@ -1,7 +1,9 @@
 const { test, after } = require('node:test')
+const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const logger = require('../utils/logger')
 
 const api = supertest(app)
 
@@ -10,6 +12,19 @@ test('notes are returned as json', async () => {
     .get('/api/notes')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('there are two notes', async () => {
+  const response = await api.get('/api/notes')
+  logger.info('Response body:', JSON.stringify(response.body, null, 2))
+  assert.strictEqual(response.body.length, 2)
+})
+
+test('the first note is about HTTP methods', async () => {
+  const response = await api.get('/api/notes')
+
+  const contents = response.body.map(e => e.content)
+  assert(contents.includes('HTML is easy'))
 })
 
 after(async () => {
