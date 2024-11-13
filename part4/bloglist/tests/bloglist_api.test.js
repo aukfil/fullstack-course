0214f86bd5a69a55cc1c156cfb767f1b90147c40
auksztol <bigiottini@gmail.com>
2024-11-13 19:21:37 +0000

    Add solution to 4.9

--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -1,10 +1,22 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./test_helper')
 const app = require('../app')
-
 const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+  
+    let blogObject = new Blog(helper.initialBlogs[0])
+    await blogObject.save()
+  
+    blogObject = new Blog(helper.initialBlogs[1])
+    await blogObject.save()
+  })
 
 test('blogs are returned as json', async () => {
   await api
---
+++ part4/bloglist/tests/test_helper.js
@@ -0,0 +1,18 @@
+// const Blog = require('../models/blog')
+
+const initialBlogs = [
+    {
+      title: 'Blog A',
+      author: 'Author A',
+      url: 'blog.com/A',
+      likes: 3
+    },
+    {
+      title: 'Blog B',
+      author: 'Author B',
+      url: 'blog.com/B',
+      likes: 5
+    }
+  ]
+
+module.exports = {initialBlogs}
