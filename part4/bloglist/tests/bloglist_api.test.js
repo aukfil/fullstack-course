9dbc81b5447a7bf2c9b9faa8203bd5e44be07c47
auksztol <bigiottini@gmail.com>
2024-11-13 19:04:59 +0000

    Add test for checking blog id

--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -1,4 +1,5 @@
 const { test, after } = require('node:test')
+const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
@@ -10,6 +11,17 @@ test('blogs are returned as json', async () => {
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('unique identifier property is named id', async () => {
+  const response = await api.get('/api/blogs')
+  console.log(response.body)
+  const blog = response.body[0]
+  assert.strictEqual(
+    typeof blog.id !== 'undefined',
+    true,
+    'blog.id should be defined'
+  )
 })
 
 after(async () => {
