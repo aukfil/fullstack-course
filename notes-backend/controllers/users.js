1267117bc20b7b6ef49225fdd02105ea195d9e37
auksztol <bigiottini@gmail.com>
2024-11-23 14:20:05 +0100

    Update notes backend

--- notes-backend/controllers/users.js
+++ notes-backend/controllers/users.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
-usersRouter.post('/', async (request, response) => {
+usersRouter.post('/', async (request, response, next) => {
   const { username, name, password } = request.body
 
   const saltRounds = 10
@@ -14,9 +14,12 @@ usersRouter.post('/', async (request, response) => {
     passwordHash,
   })
 
-  const savedUser = await user.save()
-
-  response.status(201).json(savedUser)
+  try {
+    const savedUser = await user.save()
+    response.status(201).json(savedUser)
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = usersRouter
