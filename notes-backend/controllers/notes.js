eca01a03b3e4dd32beee7fec0d062271f2233c0d
auksztol <bigiottini@gmail.com>
2024-11-29 10:26:33 +0100

    Update notes backend

--- notes-backend/controllers/notes.js
+++ notes-backend/controllers/notes.js
@@ -1,5 +1,5 @@
-const notesRouter = require('express').Router()
 const jwt = require('jsonwebtoken')
+const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
 
@@ -11,9 +11,43 @@ const getTokenFrom = request => {
   return null
 }
 
-notesRouter.get('/', async(request, response) => {
-  const notes = await Note.find({})
+notesRouter.get('/', async (request, response) => {
+  const notes = await Note
+    .find({}).populate('user', { username: 1, name: 1 })
   response.json(notes)
+})
+
+notesRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  const updatedNote = await Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  response.json(updatedNote)
+})
+
+notesRouter.post('/', async (request, response) => {
+  const body = request.body
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    user: user._id
+  })
+
+  const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
+  response.status(201).json(savedNote)
 })
 
 notesRouter.get('/:id', async (request, response) => {
@@ -29,63 +63,5 @@ notesRouter.delete('/:id', async (request, response) => {
   await Note.findByIdAndDelete(request.params.id)
   response.status(204).end()
 })
-
-notesRouter.put('/:id', (request, response, next) => {
-  const { content, important } = request.body
-
-  Note.findByIdAndUpdate(request.params.id, { content, important }, { new: true, runValidators: true, context: 'query' })
-    .then(updatedNote => {
-      response.json(updatedNote)
-    })
-    .catch(error => next(error))
-})
-
-notesRouter.post('/', async (request, response) => {
-  const body = request.body
-
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
-
-  const user = await User.findById(decodedToken.id)
-  if (!user) {
-    return response.status(400).json({ error: 'user not found' })
-  }
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    user: user.id
-  })
-
-  const savedNote = await note.save()
-  user.notes = user.notes.concat(savedNote._id)
-  await user.save()
-  response.status(201).json(savedNote)
-})
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-notesRouter.use(unknownEndpoint)
-
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  } else if (error.name ===  'JsonWebTokenError') {
-    return response.status(401).json({ error: 'token invalid' })
-  }
-
-  next(error)
-}
-
-notesRouter.use(errorHandler)
 
 module.exports = notesRouter
