cede75a980416e062038c66d3e94a366d067f665
auksztol <bigiottini@gmail.com>
2024-12-01 21:48:07 +0100

    Add user handling

--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -1,16 +1,18 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
-
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
 const Blog = require('../models/blog')
 
+describe('tests for blog posting', () => {
 beforeEach(async () => {
     await Blog.deleteMany({})
-  
+
     let blogObject = new Blog(helper.initialBlogs[0])
     await blogObject.save()
   
@@ -131,6 +133,62 @@ test('updating number of likes for a blog succeeds', async () => {
   const blogsAtEnd = await helper.blogsInDb()
   const updatedBlogInDb = blogsAtEnd.find(blog => blog.id === blogToUpdate.id)
   assert.strictEqual(updatedBlogInDb.likes, blogToUpdate.likes + 1)
+})
+
+})
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    assert(usernames.includes(newUser.username))
+  })
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert(result.body.error.includes('expected `username` to be unique'))
+
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+  })
 })
 
 after(async () => {
--- part4/bloglist/tests/test_helper.js
+++ part4/bloglist/tests/test_helper.js
@@ -1,6 +1,7 @@
 const notesRouter = require('../../../notes-backend/controllers/notes')
 const note = require('../../../notes-backend/models/note')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [
     {
@@ -22,4 +23,9 @@ const blogsInDb = async () => {
     return blogs.map(blog => blog.toJSON())
 }
 
-module.exports = { initialBlogs, blogsInDb }
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(u => u.toJSON())
+}
+
+module.exports = { initialBlogs, blogsInDb, usersInDb }
